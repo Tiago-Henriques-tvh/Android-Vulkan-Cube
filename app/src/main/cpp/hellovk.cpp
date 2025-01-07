0fea007fa2001d5994601776ffdd131a55dc48ea
Tiago-Henriques-tvh <tiriques@gmail.com>
2025-01-07 14:44:48 +0000

    final 1v rotating cube

--- app/src/main/cpp/hellovk.cpp
+++ app/src/main/cpp/hellovk.cpp
@@ -161,7 +161,7 @@ void HelloVK::createInstance() {
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Vulkan app";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 1, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_1;
